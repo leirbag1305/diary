ddbc861429e07e7b6cac30c5981c0dac4ced7318
leirbag1305 <ghfs.15@gmail.com>
2021-03-29 19:43:51 -0300

    Criado tela de listagem de eventos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from core.models import Event
 # Create your views here.
+
+
+def event_list(request):
+   #user = request.user
+    event = Event.objects.all()
+    data = {'events':event}
+    return render(request, 'diary.html', data)
